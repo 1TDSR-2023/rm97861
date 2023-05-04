76f30759b9fd14d66fd46727bcc4cb557a855b49
LucasPetroni <rm97861@fiap.com.br>
2023-05-03 22:56:12 -0300

    Button-RGB comaddEventLister Math, getByID

--- js/script.js
+++ js/script.js
@@ -5,10 +5,41 @@
 // //Regra 2: Nao alterar o seu valor durante o programa
 // const nome1 = "Ruan";
 
-var nome1 = "Petroni";
+// var nome1 = "Petroni";
 
-if (nome1 != "") {
-  var nome1 = "Geovanna";
-}
+// if (nome1 != "") {
+//   var nome1 = "Geovanna";
+// }
 
-console.log(nome1);
+// console.log(nome1);
+
+let botao = document.getElementById("meu-btn");
+// console.log(botao.textContent);
+
+botao.addEventListener("click", function () {
+  // console.log(this.textContent);
+  // Funcao Math
+  // random = retorna 1 numero aleatorio entre 0 e 1
+  // floor =  Ela arredonda um numero para baixo
+  // ceil =  Ela arredonda um numero para cima
+  // round =  Ela arredonda um numero aleatoriamente
+
+  let r = 0;
+  let g = 0;
+  let b = 0;
+  r = Math.round(Math.random() * 255);
+  g = Math.round(Math.random() * 255);
+  b = Math.round(Math.random() * 255);
+
+  // this.setAttribute("Nome do Atributo", "Valor do Atributo")
+
+  // this.setAttribute("style","background-color:rgb(" + r + "," + g + "," + b + ");");
+  this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
+});
+
+
+// Estudar 
+// querySelector
+// querySelectorAll
+// Array
+// Estruturas de repeticao
